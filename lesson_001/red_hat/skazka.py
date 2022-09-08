f2666563aa296877c7b9c6e03b260504fee706a8
dmitriish959 <80873437+dmitriish959@users.noreply.github.com>
2022-09-08 18:02:35 +0500

    Попытка разобраться с записью из нескольки фаилов txt в один

---
+++ lesson_001/red_hat/skazka.py
@@ -0,0 +1,15 @@
+# выводит текст из фаила txt
+#for i in open('01_jorney_begin.txt'):
+    #print(i.strip())
+#закрывает фаил
+#f.close()
+# Функция записи в фаил txt
+f = open('skazka.txt', 'a')
+f.write('x')
+f.close()
+
+
+
+
+
+
