e2a20dff545c75ecf4adeba0cbe8004cd2a3cb71
dmitriish959 <80873437+dmitriish959@users.noreply.github.com>
2022-10-24 23:22:53 +0500

    WIP

--- time/time.py
+++ time/time.py
@@ -1,4 +1,13 @@
-import time
-n=31536000
-time_format = time.strftime("День: %A, Время: %H:%M:%S, Месяц: %B", time.gmtime(n))
-print("Нужный формат времени:-",time_format)
+a=int(input())
+h=a//3600
+m=(a//60)%60
+s=a%60
+if m<10:
+    m=str('0'+m)
+else:
+    m=str(m)
+if s<10:
+    s=str('0'+s)
+else:
+    s=str(s)
+print(str(h)+':'+str(m)+':'+str(s))
